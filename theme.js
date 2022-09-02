730f910da9acc6c97f5f72c64dd8dd6d2eeb2063
yuu200219 <w200219200219@gmail.com>
2022-09-02 17:42:52 +0800

    margin edit

--- theme.js
+++ theme.js
@@ -2,10 +2,12 @@ const toggle = document.querySelector('.theme-mode')
 const body = document.querySelector('body')
 const topSection = document.querySelector('.top-section')
 const titlePage = document.querySelector('.title-page')
+// const workPage = document.querySelector('.work-page')
 
 toggle.onclick = function() {
     toggle.classList.toggle('dark-mode')
     body.classList.toggle('dark-background')
     topSection.classList.toggle('dark-top-section')
     titlePage.classList.toggle('dark-titlePage')
+    // workPage.classList.toggle('dark-workPage')
 }
