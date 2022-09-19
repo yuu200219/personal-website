2f19d926fabc064d8d11329baabc8eca3c78aecb
yuu200219 <w200219200219@gmail.com>
2022-09-19 15:15:10 +0800

    upate

--- theme.js
+++ theme.js
@@ -3,11 +3,14 @@ const body = document.querySelector('body')
 const topSection = document.querySelector('.top-section')
 const titlePage = document.querySelector('.title-page')
 const workPage = document.querySelector('.work-page')
+const notePage = document.querySelector('.note-page')
 
 toggle.onclick = function() {
     toggle.classList.toggle('dark-mode')
     body.classList.toggle('dark-background')
     topSection.classList.toggle('dark-top-section')
     titlePage.classList.toggle('dark-titlePage')
+
     workPage.classList.toggle('dark-workPage')
+    notePage.classList.toggle('dark-notePage')
 }
